778c72c2dcfce1e76cddaa0ced48524fadcc8277
Jani Syed <51842082+janisyed18@users.noreply.github.com>
2026-07-08 16:48:15 +0530

    Fix certificate engine protobuf runtime

--- services/certificate/src/hms_certificate/proto/certificate_pb2_grpc.py
+++ services/certificate/src/hms_certificate/proto/certificate_pb2_grpc.py
@@ -5,7 +5,7 @@ import warnings
 
 from . import certificate_pb2 as certificate__pb2
 
-GRPC_GENERATED_VERSION = '1.82.0'
+GRPC_GENERATED_VERSION = '1.81.1'
 GRPC_VERSION = grpc.__version__
 _version_not_supported = False
 
